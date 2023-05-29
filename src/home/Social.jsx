331d7e71480c3d54a5de047d7aa8bf643d1f24b9
GreyFoundation2020 <gregoryudofa@gmail.com>
2023-05-29 16:17:32 +0100

    attributes work

--- src/home/Social.jsx
+++ src/home/Social.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 export default function Social() {
   return (
     <div className="home__social">
-        <a href="" className="home__social-icon " target='_blank'>
+        <a href="#submit" className="home__social-icon " target='_blank'>
             <i className="uil uil-instagram" style={{color:"DeepPink"}}></i>
         </a>
-        <a href="" className="home__social-icon " target='_blank'>
+        <a href="#submit" className="home__social-icon " target='_blank'>
             <i class="uil uil-whatsapp-alt" style={{color:"LimeGreen"}}></i>
         </a>
-        <a href="" className="home__social-icon " target='_blank'>
+        <a href="#submit" className="home__social-icon " target='_blank'>
            <i class="uil uil-facebook-f" style={{color:"blue"}}></i>
         </a>
         
-        <a href="" className="home__social-icon " target='_blank'>
+        <a href="#submit" className="home__social-icon " target='_blank'>
             <i className="uil uil-twitter-alt" style={{color:"DodgerBlue"}}></i>
         </a>
-            <a href="" className="home__social-icon " target='_blank'>
+            <a href="#submit" className="home__social-icon " target='_blank'>
             <i className="uil uil-github-alt" style={{color:"MidnightBlue"}}></i>
         </a>
     </div>
